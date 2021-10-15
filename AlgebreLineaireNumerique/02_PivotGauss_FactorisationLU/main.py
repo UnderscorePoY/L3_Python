537157f57e57b3c8039e5f7f67d6d6cdd1785938
UnderscorePoY <71231361+UnderscorePoY@users.noreply.github.com>
2021-10-15 16:04:20 +0200

    Update main.py

--- AlgebreLineaireNumerique/02_PivotGauss_FactorisationLU/main.py
+++ AlgebreLineaireNumerique/02_PivotGauss_FactorisationLU/main.py
@@ -1,5 +1,8 @@
 from methodes_directes import *
 
+
+# [1] TEST GAUSS SANS PERMUTATION
+
 A = [[1., 0., 1.], [0., 2., -1.], [-1., 1., -2.]]
 b = [2., 1., -2.]
 
@@ -7,3 +10,15 @@ b = [2., 1., -2.]
 # b = [2., 1., -2.]
 # Résultat attendu : [1.0, 1.0, 1.0]
 print(gauss_sans_permutation(A, b))
+
+
+# [2] TEST GAUSS PIVOT PARTIEL
+
+
+A = [[0, 1, 0], [1, 1, 1], [0, 1, 2]]
+b = [-1, 2, 3]
+
+# A = [[0, 1, 0], [1, 1, 1], [0, 1, 2]]
+# b = [-1, 2, 3]
+# Résultat attendu : [1.0, -1.0, 2.0]
+print(gauss_pivot_partiel(A, b))

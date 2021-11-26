2b6dd94ee1c0ee568828af8355fa602b259e3291
UnderscorePoY <71231361+UnderscorePoY@users.noreply.github.com>
2021-11-26 12:28:11 +0100

    Add files via upload

--- MethodesNumeriques/3_approximation_integrales.py
+++ MethodesNumeriques/3_approximation_integrales.py
@@ -62,6 +62,56 @@ def Simpson_comp(f, noeuds):
     return gen_comp(f, Simpson_elem, noeuds)
 
 
+def TZ_comp_opti(f, noeuds):
+    """ Formule des trapèzes optimisée pour limiter le nombre d'évaluations de la fonction f.
+    2n => n+1 évaluations."""
+    
+    S = 0
+    n = len(noeuds)
+    vals = [f(x) for x in noeuds] # Cache les valeurs de f en les noeuds
+    
+    # terme initial
+    S = 1/2*(noeuds[1]-noeuds[0])*vals[0]
+    
+    # terme final
+    if n > 2:
+        S += 1/2*(noeuds[n-1]-noeuds[n-2])*vals[n-1]
+    
+    # termes intermédiaires
+    if n > 3:
+        for i in range(1, n-1):
+            S += (noeuds[i+1]-noeuds[i])*vals[i]
+            
+    return S
+
+def Simpson_comp_opti(f, noeuds):
+    """ Formule de Simpson optimisée pour limiter le nombre d'évaluations de la fonction f.
+    3n => 2n+1 évaluations."""
+    
+    S = 0
+    n = len(noeuds)
+    vals = [f(x) for x in noeuds] # Cache les valeurs de f en les noeuds
+    vals_mil = [f((noeuds[i]+noeuds[i+1])/2) for i in range(n-1)] # Cache les valeurs de f en les milieux des noeuds
+    
+    # terme initial
+    S = 1/6*(noeuds[1]-noeuds[0])*vals[0]
+    
+    # termes des milieux
+    for i in range(n-1):
+       S += 2/3*(noeuds[i+1]-noeuds[i])*vals_mil[i]
+    
+    # terme final   
+    if n > 2:
+        S += 1/6*(noeuds[n-1]-noeuds[n-2])*vals[n-1]
+    
+    # termes intermédiaires
+    if n > 3:
+        for i in range(1, n-1):
+            S += 1/3*(noeuds[i+1]-noeuds[i])*vals[i]
+        
+    return S
+
+
 # Utils
 
 def verif_noeuds(noeuds):
@@ -118,10 +168,15 @@ class Func:
 
 def uniform(a, b, n):
     """ Crée une liste de n noeuds uniformément répartis du segment [a,b], bornes comprises."""
-    # TODO : FIX !!!!!!!
     lst = [a+(b-a)*i/(n-1) for i in range(n)]
     return lst
 
+
+
+# SCRIPTS
+
+'''
+# Test de toutes les méthodes composées sur une fonction
 
 f = Func(lambda x: 1, 
          lambda x: x)
@@ -132,33 +187,67 @@ g = Func(lambda x: 2*x,
 h = Func(lambda x: 1/x,
          lambda x: np.log(x))
 
-
-# Script
-'''
 a, b = 1, 2
 n = 10
-noeuds = [a+(b-a)*i/n for i in range(n+1)]
+noeuds = uniform(a, b, n)
 verif_noeuds(noeuds)
 
 env_h = Test_Env(h, a, b, noeuds)
 env_h.run()
 '''
 
+
+# Test d'application de la formule des rectangles à gauches pour un nombre croissant de noeuds
+
 def j(x):
     return x**2
 
 L = [10, 20, 30, 40, 100, 200, 400, 1000, 2000]
 a, b = 0, 1
+target = 1/3 # x**2 entre 0 et 1
 
+# => rectangles à gauche
 results_RAG = []
-for n in L:
-    noeuds = [a+(b-a)*i/(n-1) for i in range(n)]
-    results_RAG.append(RAG_comp(j, noeuds))
-    
-print(results_RAG)
-    
 
-    
-    
-    
-    
+for n in L:
+    noeuds = uniform(a, b, n)
+    results_RAG.append(RAG_comp(j, noeuds))
+
+errors_RAG = [abs(target-x) for x in results_RAG]
+# print(errors_RAG)
+
+# Pas de la subdivision en fonction de l'erreur
+# plt.loglog((b-a)/np.array(L), errors_RAG)
+
+# => Simpson
+
+# TODO : FIX
+results_Simpson = []
+
+for n in L:
+    noeuds = uniform(a, b, n)
+    results_Simpson.append(Simpson_comp_opti(j, noeuds))
+
+errors_Simpson = [abs(target-x) for x in results_Simpson]
+print(errors_Simpson)
+
+# Pas de la subdivision en fonction de l'erreur
+plt.loglog((b-a)/np.array(L), errors_Simpson)
+
+'''
+# Vérifications des formules composées optimisées
+
+def j(x):
+    return x**2
+
+a, b = 0, 1
+n = 10
+noeuds = uniform(a, b, n)
+print("Vérification des formules des trapèzes")
+print("Méthode non optimisée : ", TZ_comp(j, noeuds))
+print("Méthode optimisée : ", TZ_comp_opti(j, noeuds))
+
+print("Vérification des formules de Simpson")
+print("Méthode non optimisée : ", Simpson_comp(j, noeuds))
+print("Méthode optimisée : ", Simpson_comp_opti(j, noeuds))
+'''
